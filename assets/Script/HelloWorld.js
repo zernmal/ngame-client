305b71186d052e251cd2d825838a8254facecc75
zernmal <zernmal@foxmail.com>
2018-02-06 11:45:41 +0800

    feat: no socket.io

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -14,11 +14,24 @@ cc.Class({
     onLoad: function () {
         this.label.string = this.text;
         const io = window.io;
+        
         console.log('try to connect')
-        const socket = io('ws://localhost:5001');
-        socket.on('connected', function() {
-            console.log('connected --- ');
-        });
+        const ws = new WebSocket("ws://127.0.0.1:5001");
+        ws.onopen = function (event) {
+            ws.send('-------');
+            console.log("Send Text WS was opened.");
+        };
+        ws.onmessage = function (event) {
+            console.log("response text msg: " + event.data);
+        };
+        ws.onerror = function (event) {
+            console.log("Send Text fired an error");
+        };
+        ws.onclose = function (event) {
+            console.log("WebSocket instance closed.");
+        };
+
+
     },
 
     // called every frame
